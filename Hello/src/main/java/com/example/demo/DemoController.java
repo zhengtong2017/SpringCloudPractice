8bb7b9b0e66a3f783228e6f93f17712c9b1e3635
zhengtong <skdzhengtong@163.com>
2018-08-09 22:53:48 +0800

    eureka study

--- Hello/src/main/java/com/example/demo/DemoController.java
+++ Hello/src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class DemoController {
+    private final Logger logger = Logger.getLogger("com.example.demo.DemoController");
 
-    @RequestMapping("/hello")
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String index(){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello,host:"+instance.getHost() + ",service_id:"+instance.getServiceId());
         return "hello, world!";
     }
 }
